2c5341344d55fb374b109c3c06e52264b6db210a
Dmitry Ivanov <dimannisik1@gmail.com>
2022-11-07 01:56:25 +0700

    feat: implement merge styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const fs = require("fs");
+const { readdir, unlink } = require("fs/promises");
+
+async function styleBundler(pathStyles, pathBundle, nameBundle) {
+  const filesProjectDist = await readdir(pathBundle, { withFileTypes: true });
+  for (const file of filesProjectDist) {
+    if (file.isFile()) {
+      if (file.name === nameBundle) {
+        await unlink(path.join(pathBundle, nameBundle));
+      }
+    }
+  }
+
+  const filesStyles = await readdir(pathStyles, { withFileTypes: true });
+  const output = fs.createWriteStream(path.join(pathBundle, nameBundle));
+  for (const file of filesStyles) {
+    if (file.isFile()) {
+      const extension = file.name.split(".")[file.name.split(".").length - 1];
+      if (extension === "css") {
+        const stream = fs.createReadStream(
+          path.join(pathStyles, file.name),
+          "utf-8"
+        );
+        stream.pipe(output);
+      }
+    }
+  }
+}
+
+styleBundler(
+  path.join(__dirname, "styles"),
+  path.join(__dirname, "project-dist"),
+  "bundle.css"
+);
